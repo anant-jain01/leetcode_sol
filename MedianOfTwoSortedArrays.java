af30a56f15787f46e8760e2978353cab5d56655e
anant-jain01 <jain.anant609@gmail.com>
2025-02-07 10:02:02 +0530

    modified:   MedianOfTwoSortedArrays.java

--- MedianOfTwoSortedArrays.java
+++ MedianOfTwoSortedArrays.java
@@ -1,1 +1,10 @@
-/* */
+/*class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int l_1=nums1.length;int l_2=nums2.length;
+       int[] arr=new int[l_1+l_2];
+       int i=0;int j=0;
+       while(i<l_1){
+        arr[i]=nums1[i];
+        i++;
+       }
+        */
