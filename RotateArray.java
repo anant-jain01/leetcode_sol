fde1dc1fe13f81e74c763e8d3425d8e3524dacbe
anant-jain01 <jain.anant609@gmail.com>
2025-02-14 00:34:49 +0530

    modified:   RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -1,3 +1,20 @@
 public class RotateArray {
-    /*class solution{}*/
+    class Solution {
+        public int[] reverse(int[] nums,int left,int right){
+            while (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+            return nums;
+        }
+        public void rotate(int[] nums, int k) {
+            k%=nums.length;
+            reverse(nums,0,nums.length-1);
+            reverse(nums,k,nums.length-1);
+            reverse(nums,0,k-1);
+        }
+    }
 }
