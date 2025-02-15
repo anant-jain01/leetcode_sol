7515c7fca04685ca2962327b5cb39dcda636fd1d
anant-jain01 <jain.anant609@gmail.com>
2025-02-15 14:22:22 +0530

    modified:   function/bubbleSort.java

--- function/bubbleSort.java
+++ function/bubbleSort.java
@@ -1,9 +1,10 @@
 package function;
+import java.util.Scanner;
 
 public class bubbleSort {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int i=sc.nextInt;int[] arr=new int[i];
+        int i=sc.nextInt();int[] arr=new int[i];
         while(i>0){
             arr[i]=sc.nextInt;
         }
