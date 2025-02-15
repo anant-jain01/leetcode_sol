781f818a0fa95f0a137154ce832b7bce6e283ca9
anant-jain01 <jain.anant609@gmail.com>
2025-02-15 14:21:35 +0530

    modified:   function/bubbleSort.java

--- function/bubbleSort.java
+++ function/bubbleSort.java
@@ -2,5 +2,8 @@ package function;
 
 public class bubbleSort {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt;int[] arr=new int[i];
+        
     }
 }
