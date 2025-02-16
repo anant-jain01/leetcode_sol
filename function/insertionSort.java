8513f54607b561560a713908c8bfe185ded88242
anant-jain01 <jain.anant609@gmail.com>
2025-02-17 00:06:12 +0530

    modified:   function/insertionSort.java

--- function/insertionSort.java
+++ function/insertionSort.java
@@ -1,8 +1,15 @@
 package function;
 
 public class insertionSort {
-    public static void main(String[] args) {
-        try(Scanner sc=new Scanner(System.in)){
-            System.out.print("no of elements:");
-            int n=sc.nextInt();
-           
+    j++) {
+                for (int i = j; i > 0 ; i--){
+                    if(arr[i]<arr[i-1]){
+                        int temp=arr[i];
+                        arr[i]=arr[i-1];
+                        arr[i-1]=temp;
+                    }
+                }
+            }
+            return arr;
+    }
+}
