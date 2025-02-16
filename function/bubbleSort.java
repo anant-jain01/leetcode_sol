e8ad0eaff9c2a407928bb18c39b6f29910a95464
anant-jain01 <jain.anant609@gmail.com>
2025-02-16 15:47:29 +0530

    modified:   function/bubbleSort.java

--- function/bubbleSort.java
+++ function/bubbleSort.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class bubbleSort {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        try(Scanner sc=new Scanner(System.in)){
         int i=sc.nextInt();
         int[] arr=new int[i];
         for (int j = 0; j < i; j++) {
@@ -15,7 +15,7 @@ public class bubbleSort {
         for (int j = 0; j < num.length; j++) {
             System.out.println(num[j]);
         }
-    
+    }
     }
     public static int[] bubble(int[] arr){
         for(int j=0;j<arr.length;j++){
