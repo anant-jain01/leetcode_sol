10d982c668d663236d1e54780a94560db54c7766
anant-jain01 <jain.anant609@gmail.com>
2025-02-17 00:01:40 +0530

    modified:   function/insertionSort.java

--- function/insertionSort.java
+++ function/insertionSort.java
@@ -3,6 +3,13 @@ package function;
 public class insertionSort {
     public static void main(String[] args) {
         try(Scanner sc=new Scanner(System.in)){
+            System.out.print("no of elements:");
+            int n=sc.nextInt();
+            int[] arr=new int[n];
             
+                    }
+                }
+            }
+            return arr;
     }
 }
