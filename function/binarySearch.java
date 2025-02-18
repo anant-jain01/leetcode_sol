6fa43671a984432006922d23ee3a7de0847d8de1
anant-jain01 <jain.anant609@gmail.com>
2025-02-18 16:52:27 +0530

    new file:   function/binarySearch.java

---
+++ function/binarySearch.java
@@ -0,0 +1,13 @@
+package function;
+import java.util.Scanner;
+public class binarySearch {
+    public static void main(String[] args) {
+    try(Scanner sc=new Scanner(System.in)){
+        int x=sc.nextInt();
+        int[] arr=new int[x];
+        while(x>=0){
+
+        }
+    }
+}
+}
