8f1705ee0e2bbb8215022477699f64b5c4499508
anant-jain01 <jain.anant609@gmail.com>
2025-05-04 20:26:24 +0530

    new file:   Missing Number.java

---
+++ Missing Number.java
@@ -0,0 +1,11 @@
+public class Missing Number {
+  /*class Solution {
+    public int missingNumber(int[] nums) {
+        int originalSum=(nums.length*(nums.length+1))/2;
+        for(int num:nums){
+            originalSum-=num;
+        }
+        return originalSum;
+    }
+} */
+}
