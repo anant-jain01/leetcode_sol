91c8e5a6158e882cf6be1b9ffdfa316280fcf4a7
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2025-10-13 22:19:43 +0530

    new file:   CreateTargetArrayintheGivenOrder.java

---
+++ CreateTargetArrayintheGivenOrder.java
@@ -0,0 +1,11 @@
+/*class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+     ArrayList<Integer> lst=new ArrayList<>();
+     int[] arr=new int[nums.length];
+     for( int i=0;i<nums.length;i++)
+        lst.add(index[i],nums[i]);
+     for(int i=0;i<lst.size();i++)
+     arr[i]=lst.get(i);
+     return arr;
+    }
+} */
