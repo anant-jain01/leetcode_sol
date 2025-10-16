7aba75c6e04f8f9d6687674612310b83daa17bf7
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2025-10-16 12:19:33 +0530

    new file:   AddtoArray.java

---
+++ TransposeMatrix.java
@@ -0,0 +1,12 @@
+/*class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int rows=matrix.length;int colomn=matrix[0].length;
+        int[][] arr=new int[colomn][rows];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                arr[j][i]=matrix[i][j];
+            }
+        }
+        return arr;
+    }
+} */
